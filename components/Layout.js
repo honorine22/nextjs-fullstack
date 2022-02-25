685c63a00747e2d246f42663142c65d00c9c975e
honorine22 <higiranez@gmail.com>
2022-02-25 08:34:36 +0200

    Started Frontend

---
+++ components/Layout.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Head from 'next/head'
+import { Header } from './layout/Header'
+import Footer from './layout/Footer'
+const Layout = ({ children, title = 'Book Best Hotels for your Holiday' }) => {
+    return (
+        <div className='dark:bg-gray-800'>
+            <Head>
+                <title>{title}</title>
+                <meta charSet='utf-8' />
+                <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+            </Head>
+            <Header />
+            {children}
+            <Footer />
+        </div>
+    )
+}
+
+export default Layout
